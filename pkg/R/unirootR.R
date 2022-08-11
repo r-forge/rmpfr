2ca8c69e6ec05b3d1be2c3da7a6625e0f5f8ff19
mmaechler (Martin Maechler) <maechler@r-project.org>
2022-08-11 16:21:36 +0000

    provide qnormI(): accurate qnorm() via unirootR()
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rmpfr@369 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/unirootR.R
+++ pkg/R/unirootR.R
@@ -12,8 +12,8 @@ unirootR <- function(f, interval, ...,
 		     f.lower = f(lower, ...), f.upper = f(upper, ...),
                      extendInt = c("no", "yes", "downX", "upX"),
                      trace = 0,
-		     verbose = as.logical(trace),
-		     tol = .Machine$double.eps^0.25, maxiter = 1000,
+		     verbose = as.logical(trace), verbDigits = max(3, min(20, -log10(tol)/2)),
+		     tol = .Machine$double.eps^0.25, maxiter = 1000L,
                      check.conv = FALSE,
                      ## Rmpfr-only:
                      warn.no.convergence = !check.conv,
@@ -29,8 +29,8 @@ unirootR <- function(f, interval, ...,
     if(is.na(.N(f.lower))) stop("f.lower = f(lower) is NA")
     if(is.na(.N(f.upper))) stop("f.upper = f(upper) is NA")
 
-    ff <- f.lower * f.upper # used later also for mpfr-checking
-    form <- if(inherits(ff, "mpfr")) function(x) format(x, drop0trailing=TRUE) else format
+    form  <- function(x, digits = verbDigits) format(x, digits=digits, drop0trailing=TRUE)
+    formI <- function(x, di = getOption("digits")) format(x, digits=di, drop0trailing=TRUE)
     Sig <- switch(match.arg(extendInt),
 		  "yes" = NULL,
 		  "downX"= -1,
@@ -52,7 +52,7 @@ unirootR <- function(f, interval, ...,
     if(doX) { ## extend the interval = [lower, upper]
 	if(trace)
 	    cat(sprintf("search {extendInt=\"%s\", Sig=%s} in [%s,%s]%s",
-                        extendInt, form(Sig), form(lower), form(upper),
+                        extendInt, formI(Sig), formI(lower), formI(upper),
 			if(trace >= 2)"\n" else " ... "))
 	Delta <- function(u) 0.01* pmax(1e-4, abs(u)) ## <-- FIXME? [= R's uniroot() for double]
         it <- 0L
@@ -91,7 +91,7 @@ unirootR <- function(f, interval, ...,
 		    stop(gettextf("no sign change found in %d iterations", it-1),
 			 domain=NA)
 		f.lower <- f(lower <- lower - delta, ...)
-		if(trace >= 2) cat(sprintf(" .. modified lower: %s, f(.)=%s\n", form(lower), form(f.lower)))
+		if(trace >= 2) cat(sprintf(" .. modified lower: %s, f(.)=%s\n", formI(lower), formI(f.lower)))
 		delta <- 2 * delta
 	    }
 	    delta <- Delta(upper)
@@ -100,22 +100,23 @@ unirootR <- function(f, interval, ...,
 		    stop(gettextf("no sign change found in %d iterations", it-1),
 			 domain=NA)
 		f.upper <- f(upper <- upper + delta, ...)
-		if(trace >= 2) cat(sprintf(" .. modified upper: %s, f(.)=%s\n", form(upper), form(f.upper)))
+		if(trace >= 2) cat(sprintf(" .. modified upper: %s, f(.)=%s\n", formI(upper), formI(f.upper)))
 		delta <- 2 * delta
 	    }
 	}
 	if(trace && trace < 2)
-            cat(sprintf("extended to [%s, %s] in %d steps\n", form(lower), form(upper), it))
+            cat(sprintf("extended to [%s, %s] in %d steps\n", formI(lower), formI(upper), it))
     }
     if(!isTRUE(as.vector(sign(f.lower) * sign(f.upper) <= 0)))
 	stop(if(doX)
 	"did not succeed extending the interval endpoints for f(lower) * f(upper) <= 0"
 	     else sprintf("f() values at end points = (%s, %s) not of opposite sign",
-                          form(f.lower), form(f.upper)))
+                          formI(f.lower), formI(f.upper)))
 
     if(is.null(epsC) || is.na(epsC)) {
 	## determine 'epsC'  ``the achievable Machine precision''
 	## -- given the class of f.lower, f.upper
+        ff <- f.lower * f.upper
 	if(is.double(ff)) epsC <- .Machine$double.eps
 	else if(is(ff, "mpfr"))
 	    epsC <- 2^-min(getPrec(f.lower), getPrec(f.upper))
@@ -152,12 +153,11 @@ unirootR <- function(f, interval, ...,
     c <- a
     fc <- fa
     ## First test if we have found a root at an endpoint
-    maxit <- maxiter + 2 # count evaluations as maxiter-maxit
+    maxit <- maxiter + 2L # count evaluations as maxiter-maxit
     converged <- FALSE
 
     while(!converged && maxit > 0) { ##---- Main iteration loop ------------------------------
-
-	if (verbose) cat("Top of iteration, maxit=",maxit,"\n")
+	if (verbose) cat("Iteration >>>", maxiter+3L-maxit, "<<< ;")
 	d.prev <- b-a
 	## Distance from the last but one to the last approximation	*/
 	##double tol.2;		       ## Actual tolerance		*/
@@ -177,9 +177,8 @@ unirootR <- function(f, interval, ...,
 	    fc <- fa
 	}
 	tol.2 <- 2*epsC*abs(b) + tol/2
-	if (verbose) cat("tol.2(epsC,b) = ",.N(tol.2),"\n")
 	d.new <- (c-b)/2 # bisection
-	if (verbose) cat("d.new= ",.N(d.new),"\n")
+	if (verbose) cat("tol.2(epsC,b) = ",.N(tol.2), "; d.new= ",.N(d.new),"\n", sep="")
 
 	## converged <- (abs(d.new) <= tol.2 && is.finite(fb))  ||  fb == 0
 	converged <- (abs(d.new) <= tol.2)  ||  fb == 0
@@ -236,15 +235,16 @@ unirootR <- function(f, interval, ...,
 	    fa <- fb ## Save the previous approx. */
 	    b <- b + d.new
 	    fb <- f(b, ...)
-            if (verbose) cat(sprintf("new f(b) = %s; ", form(fb)))
+            if (verbose) cat(sprintf("new f(b=%s) = %s;\n", form(b), form(fb)))
 	    maxit <- maxit-1
 	    ## Do step to a new approxim. */
 	    if( ((fb > 0) && (fc > 0)) || ((fb < 0) && (fc < 0)) ) {
-		if (verbose) cat(sprintf("make c to have sign opposite to b: f(c)=%s\n", form(fa)))
+		if (verbose) cat(sprintf("  make c:=a=%s to have sign opposite to b: f(c)=%s\n",
+                                         form(a), form(fa)))
 		## Adjust c for it to have a sign opposite to that of b */
 		c <- a
 		fc <- fa
-            } else if(verbose) cat("\n")
+            }
 
 	}## else not converged
 
@@ -265,4 +265,98 @@ unirootR <- function(f, interval, ...,
     }
     list(root = val[["root"]], f.root = f(val[["root"]], ...),
 	 iter = iter, estim.prec = .N(val[["rtol"]]), converged = converged)
+} ## {unirootR}
+
+
+### qnorm() via inversion of pnorm()  by  unirootR() ==========================
+"
+qnorm(p) == q   <==>  pnorm(q) == p
+"
+
+##====== TODO: Use good i.e. *tight* inequalities for  Phi(x) ..  which are invertible
+##       ===== to (still tight) inequalities for Phi^{-1}(x) == qnorm(x)
+## ===> get good *start* interval for unirootR()
+
+## qnorm  (p, mean = 0, sd = 1, lower.tail = TRUE, log.p = FALSE)
+qnormI <- function(p, mean = 0, sd = 1, lower.tail = TRUE, log.p = FALSE,
+                   trace = 0, verbose = as.logical(trace), # <- identical defaults as unirootR()
+                   tol, # for "base" = .Machine$double.eps^0.25, but here use getPrec()
+                   give.full = FALSE,
+                   ...) # <- arguments to pass to unirootR()
+{
+    zFun <- function(q) pnorm(q, mean=mean, sd=sd, lower.tail=lower.tail, log.p=log.p) - p.
+    if(missing(tol) || !is.finite(tol)) {
+        prec <- max(getPrec(c(p, mean, sd)))
+        tol <- 2^-(prec - 1)
+    } else
+        prec <- as.integer(ceiling(1 - log2(tol)))
+    useM <- (prec > 53) # if true, use mpfr
+    ##
+    if(verbose) cat(sprintf("prec=%d ==> useM=%s:\n", prec, format(useM)))
+    verbDigs <- if(any(is.vd <- "verbDigits" == ...names()))
+                 ...elt(which(is.vd))
+             else max(3, min(20, -log10(tol)/2))
+    if(useM) { ## This **IS** important here:
+        old_eranges <- .mpfr_erange() # typically -/+ 2^30
+        myERng <- (1-2^-52) * .mpfr_erange(c("min.emin","max.emax"))
+        if(!isTRUE(all.equal(myERng, old_eranges))) {
+            .mpfr_erange_set(value = myERng)
+            on.exit( .mpfr_erange_set( , old_eranges) )
+        }
+    }
+    sgn <- if(lower.tail) -1 else 1
+    INf <- if(lower.tail) Inf else -Inf
+    ## "start"-interval for unirootR() --- see 'TODO' .. *tight* .. above <<<<<<<<
+    ## Notably for the relevant (p <<< -1, log.p=TRUE) case !
+    qnInt <-
+        if(log.p) {
+            Pi <- if(useM) Const("pi", prec) else pi
+            function(p) { s2 <- -2*p
+                if(useM && !inherits(s2, "mpfr")) s2 <- mpfr(s2, precBits = prec)
+                xs1 <- s2 - log(2*Pi*s2)
+                if(p < -54) {
+                    qn <- sqrt(s2 - log(2*Pi*xs1)); e <- 1e-4
+                } else if(p <= -15) {
+                    qn <- sqrt(s2 - log(2*Pi*xs1) - 1/(2 + xs1)); e <- 1e-3
+                } else { # p >= -15
+                    qn <- stats__qnorm(asNumeric(p), log.p=TRUE)
+                    ## FIXME: not good enough, e.g., for  p = mpfr(-1e-5, 128)
+                    e <- 1e-2
+                }
+                qn*(sgn + c(-e,e))
+            }
+        } else { ## log.p is FALSE
+            Id <- if(useM) function(.) mpfr(., precBits = prec) else identity
+            function(p) { q <- stats__qnorm(asNumeric(p), lower.tail=lower.tail)
+                Id(if(abs(q) < 1e-3) c(-2,2)*1e-3 else c(.99, 1.01) * q)
+            }
+        }
+    ## Deal with prob in {0, 1} which correspond to quantiles -/+ Inf :
+    ## idea from {DPQ}'s .D_0 and .D_1 :
+    .p_1 <- as.integer(!log.p)
+    .p_0 <- if (log.p) -Inf else 0
+    r <- if(give.full) vector("list", length(p))
+         else if(useM) mpfr(p, precBits=prec)
+         else p
+    for(ip in seq_along(p)) {
+        p. <- p[ip]
+        if(verbose) cat(sprintf("p. = p[ip=%d] = %s:\n", ip,
+                                format(p., digits = verbDigs, drop0trailing=TRUE)))
+        ri <-
+            if (p. == .p_1)
+                INf
+            else if (p. == .p_0)
+                -INf
+            else if(is.na(p.) || p. > .p_1 || p. < .p_0)
+                NaN
+            else { ## zFun() uses p.
+                ur <- unirootR(zFun, interval = qnInt(p.),
+                               extendInt = if(lower.tail) "upX" else "downX",
+                               trace=trace, verbose=verbose, tol=tol,
+                               ...) # verbDigits, maxiter, check.conv, warn.no.convergence, epsC
+                if(give.full) ur else ur$root
+            }
+        if(give.full) r[[ip]] <- ri else r[ip] <- ri
+    }
+    r
 }
